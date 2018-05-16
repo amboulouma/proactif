99e1144cd0aa37a785684482c99abfe2459a7698
aminemboulouma <amine@boulouma.com>
2018-05-16 16:40:16 +0200

    Added exceptions

--- src/main/java/Actions/Action.java
+++ src/main/java/Actions/Action.java
@@ -4,11 +4,23 @@
  * and open the template in the editor.
  */
 package Actions;
-
+import exception.*;
+import java.text.ParseException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 /**
  *
- * @author cflorant
+ * @author Amine Boulouma, Clément Florant
  */
 public abstract class Action {
-    public abstract boolean execute();
+    public static final String RESULTS_FIELD = "results";
+    
+    
+    public abstract void execute(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, NotLoggedException, SignUpException, 
+            NullAvailableProductException, ClientNullException, ConnectionFailException, 
+            IncompatibleTypeException, MissingInformationException, InfoClientUpdateException, 
+            ParseException;
+    
 }
